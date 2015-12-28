a92f00200cc7b8787170795c6813e02ea8aea4e0
yxl040840219 <yxl040840219@126.com>
2015-12-28 10:11:32 +0800

    update

--- src/main/java/org/apache/hadoop/mapred/QueueHandlerHiveDriverRunHook.java
+++ src/main/java/org/apache/hadoop/mapred/QueueHandlerHiveDriverRunHook.java
@@ -18,7 +18,7 @@ public class QueueHandlerHiveDriverRunHook implements HiveDriverRunHook {
 	private static final String MR_QUEUE_NAME_PROPERTY = "mapred.job.queue.name";
 	private static final String TEZ_QUEUE_NAME_PROPERTY = "tez.queue.name";
 	private static final String HIVE_EXECUTION_ENGINE_PROPERTY = "hive.execution.engine";
-	
+
 	private static final Log LOG = LogFactory
 			.getLog(QueueHandlerHiveDriverRunHook.class);
 
@@ -31,52 +31,47 @@ public class QueueHandlerHiveDriverRunHook implements HiveDriverRunHook {
 	public void preDriverRun(HiveDriverRunHookContext context) throws Exception {
 
 		HiveConf config = (HiveConf) context.getConf();
-		
-		//check if hive execution engine is set to tez. If so, queue name property should be tez.queue.name.
+
+		// check if hive execution engine is set to tez. If so, queue name
+		// property should be tez.queue.name.
 		String queue_property = MR_QUEUE_NAME_PROPERTY;
-		String hiveExecEngine = config.get(QueueHandlerHiveDriverRunHook.HIVE_EXECUTION_ENGINE_PROPERTY);
-		
-		if(hiveExecEngine!=null && hiveExecEngine.equalsIgnoreCase("tez")){
+		String hiveExecEngine = config
+				.get(QueueHandlerHiveDriverRunHook.HIVE_EXECUTION_ENGINE_PROPERTY);
+
+		if (hiveExecEngine != null && hiveExecEngine.equalsIgnoreCase("tez")) {
 			queue_property = TEZ_QUEUE_NAME_PROPERTY;
 		}
-		
+
 		String queue = config.get(queue_property);
-		
+
 		// if queue name is specified as default, try to find a better qualified
 		// queue
-		if (queue==null || "default".equals(queue)) {
-			String newQueue = getQualifiedQueue(context.getConf(), queue);
-			if (newQueue != null && !newQueue.equalsIgnoreCase(queue)) {
-				config.set(queue_property, newQueue);
-				LOG.info("queue name overriden to " + queue
-						+ " From default for the user " + config.getUser());
-			}
+		// if (queue==null || "default".equals(queue)) {
+		String newQueue = getQualifiedQueue(context.getConf(), queue);
+		if (newQueue != null && !newQueue.equalsIgnoreCase(queue)) {
+			config.set(queue_property, newQueue);
+			LOG.info("queue name overriden to " + queue
+					+ " From default for the user " + config.getUser());
+			// }
 		}
 	}
 
 	private String getQualifiedQueue(Configuration config, String queueInp) {
 
-		String queue = queueInp;
+		String queue = null;
 		JobConf job = new JobConf(config);
 		JobClient jobClient;
 		try {
 			jobClient = new JobClient(job);
 			QueueAclsInfo[] infos = jobClient.getQueueAclsForCurrentUser();
-			
-			LOG.info("-----queue-----");
-			for(QueueAclsInfo info:infos){
-				LOG.info("QueueAclsInfo:" + info.getQueueName());
-			}
-			LOG.info("-----queue-----");
-
+			// first check default queue
 			for (QueueAclsInfo info : infos) {
-				String name = info.getQueueName().toLowerCase();
-				if (name != null && name.indexOf("root") == -1) {
-					LOG.debug("Checking acl info for queue:" + name);
-					boolean qualified = false;
+				String name = info.getQueueName();
+				boolean qualified = false;
+
+				if ("default".equals(name)) {
+					LOG.info("优先判断 default Queue");
 					String[] ops = info.getOperations();
-					LOG.debug("Count Allowed operations:" + ops.length);
-					
 					if (ops != null && ops.length > 0) {
 						for (String op : ops) {
 							if ("SUBMIT_APPLICATIONS".equals(op)) {
@@ -85,16 +80,41 @@ public class QueueHandlerHiveDriverRunHook implements HiveDriverRunHook {
 							}
 						}
 					}
+				}
+				if (qualified) {
+					queue = name;
+					break;
+				}
+			}
 
-					if (qualified) {
-						queue = name;
-						break;
+			if (queue == null || queue.length() == 0) {
+				for (QueueAclsInfo info : infos) {
+					String name = info.getQueueName();
+					// exclude root queue
+					if (name != null && name.indexOf("root") == -1) {
+						boolean qualified = false;
+						String[] ops = info.getOperations();
+						if (ops != null && ops.length > 0) {
+							for (String op : ops) {
+								if ("SUBMIT_APPLICATIONS".equals(op)) {
+									qualified = true;
+									break;
+								}
+							}
+						}
+						if (qualified) {
+							queue = name;
+							break;
+						}
 					}
 				}
 			}
+
 		} catch (IOException e) {
-			//Do Nothing. Ignore the error and let query run in the default queue
-			LOG.error("Ignoring error while executing the pre driver run hook. error occured during the queue determination.",e);;
+			LOG.error(
+					"Ignoring error while executing the pre driver run hook. error occured during the queue determination.",
+					e);
+			;
 		}
 		return queue;
 	}
