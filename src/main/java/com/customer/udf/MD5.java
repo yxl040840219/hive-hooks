dd9ce974deaeacb3e6c7e203be6e775e7a20a4c9
yxl040840219 <yxl040840219@126.com>
2016-10-21 14:45:10 +0800

    update

--- src/main/java/com/customer/udf/MD5.java
+++ src/main/java/com/customer/udf/MD5.java
@@ -5,6 +5,9 @@ import com.google.common.hash.Hashing;
 import org.apache.hadoop.hive.ql.exec.UDF;
 /**
  * Hive自定义UDF，计算md5值
+ * hadoop fs -copyFromLocal hive-custom-0.0.1.jar /data/hive/udf/
+ * create function md5 as "com.customer.udf.MD5" using jar "hdfs://hadoop-cluster/data/hive/udf/hive-custom-0.0.1.jar" ;
+ * select md5(concat(id,name)) from t_test ;
  */
 public class MD5 extends UDF {
     /**
