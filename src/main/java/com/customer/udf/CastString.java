06738c926805eeb228f6237aa023acf214f86757
yxl040840219 <yxl040840219@126.com>
2016-10-24 16:28:18 +0800

    update

---
+++ src/main/java/com/customer/udf/CastString.java
@@ -0,0 +1,16 @@
+package com.customer.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class CastString extends UDF {
+
+    public String evaluate(final Object o) {
+        if(o == null){
+        	 	return "" ;
+        }
+        else{
+         	return String.valueOf(o);
+        }
+    }
+	
+}
