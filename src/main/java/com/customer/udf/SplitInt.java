cc0e0207dae2e0d8359ec3e3f6b524e21f5a4d49
yxl040840219 <yxl040840219@126.com>
2016-11-25 17:06:46 +0800

    update

--- src/main/java/com/customer/udf/SplitInt.java
+++ src/main/java/com/customer/udf/SplitInt.java
@@ -2,27 +2,64 @@ package com.customer.udf;
 
 import java.util.ArrayList;
 
-import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 
-class SplitInt extends UDF{
-	
-    public ArrayList<IntWritable> evaluate(final Object arg1,final Object arg2) {
+class SplitInt extends GenericUDF {
+	private transient ObjectInspectorConverters.Converter[] converters;
 
-    	    ArrayList<IntWritable> result = new ArrayList<IntWritable>();
+	@Override
+	public ObjectInspector initialize(ObjectInspector[] arguments)
+			throws UDFArgumentException {
+		if (arguments.length != 2) {
+			throw new UDFArgumentLengthException(
+					"The function SPLIT(s, regexp) takes exactly 2 arguments.");
+		}
 
-    	    for (String str : arg1.toString().split(arg2.toString(), -1)) {
-    	      result.add(new IntWritable(Integer.parseInt(str.trim())));
-    	    }
-    	    
-    	    return result;
-}
-    
-    
-  public static void main(String[] args) {
-	   SplitInt si = new SplitInt();
-	   System.out.println(si.evaluate("1,2,3",","));
-}
-    
-	
+		converters = new ObjectInspectorConverters.Converter[arguments.length];
+		for (int i = 0; i < arguments.length; i++) {
+			converters[i] = ObjectInspectorConverters
+					.getConverter(
+							arguments[i],
+							PrimitiveObjectInspectorFactory.writableStringObjectInspector);
+		}
+
+		return ObjectInspectorFactory
+				.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.writableStringObjectInspector);
+	}
+
+	@Override
+	public Object evaluate(DeferredObject[] arguments) throws HiveException {
+		assert (arguments.length == 2);
+
+		if (arguments[0].get() == null || arguments[1].get() == null) {
+			return null;
+		}
+
+		Text s = (Text) converters[0].convert(arguments[0].get());
+		Text regex = (Text) converters[1].convert(arguments[1].get());
+
+		ArrayList<IntWritable> result = new ArrayList<IntWritable>();
+
+		for (String str : s.toString().split(regex.toString(), -1)) {
+			result.add(new IntWritable(Integer.parseInt(str.trim())));
+		}
+
+		return result;
+	}
+
+	@Override
+	public String getDisplayString(String[] children) {
+		assert (children.length == 2);
+		return getStandardDisplayString("split_int", children);
+	}
+
 }
