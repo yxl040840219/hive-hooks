438b0eb8019e581c8b529f6dc931be01b668919a
yxl040840219 <yxl040840219@126.com>
2016-11-25 16:58:48 +0800

    update

--- src/main/java/com/customer/udf/SplitInt.java
+++ src/main/java/com/customer/udf/SplitInt.java
@@ -7,15 +7,22 @@ import org.apache.hadoop.io.IntWritable;
 
 class SplitInt extends UDF{
 	
-    public ArrayList<IntWritable> evaluate(final String arg1,final String arg2) {
+    public ArrayList<IntWritable> evaluate(final Object arg1,final Object arg2) {
 
     	    ArrayList<IntWritable> result = new ArrayList<IntWritable>();
 
     	    for (String str : arg1.toString().split(arg2.toString(), -1)) {
-    	      result.add(new IntWritable(Integer.parseInt(str)));
+    	      result.add(new IntWritable(Integer.parseInt(str.trim())));
     	    }
     	    
     	    return result;
 }
+    
+    
+  public static void main(String[] args) {
+	   SplitInt si = new SplitInt();
+	   System.out.println(si.evaluate("1,2,3",","));
+}
+    
 	
 }
