f864fd628e0436341814b78540f8193c126ab7f1
yxl040840219 <yxl040840219@126.com>
2017-07-11 18:49:47 +0800

    md5 可变参数

--- src/main/java/com/customer/udf/MD5.java
+++ src/main/java/com/customer/udf/MD5.java
@@ -3,21 +3,49 @@ package com.customer.udf;
 import com.google.common.base.Strings;
 import com.google.common.hash.Hashing;
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
 /**
  * Hive自定义UDF，计算md5值
  * hadoop fs -copyFromLocal hive-custom-0.0.1.jar /data/hive/udf/
  * create function md5 as "com.customer.udf.MD5" using jar "hdfs://hadoop-cluster/data/hive/udf/hive-custom-0.0.1.jar" ;
  * select md5(concat(id,name)) from t_test ;
  */
-public class MD5 extends UDF {
+public class MD5 extends GenericUDF {
+
+
+    private transient ObjectInspector[] argumentOIs;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentTypeException {
+
+        argumentOIs = arguments;
+        return PrimitiveObjectInspectorFactory.writableIntObjectInspector;
+    }
+
+
+    private final Text result = new Text();
+
     /**
-     * @param s 传入参数
+     * @param arguments 传入参数
      * @return md5值
      */
-    public String evaluate(final String s) {
-        if(Strings.isNullOrEmpty(s.trim())){
-            return null;
+    public Object evaluate(DeferredObject[] arguments) {
+        StringBuffer sb = new StringBuffer() ;
+        for (int i = 0; i < arguments.length; i++) {
+             sb.append(arguments[i].toString()) ;
         }
-        return Hashing.md5().hashString(s.trim()).toString();
+        result.set(Hashing.md5().hashString(sb.toString()).toString());
+        return result ;
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return getStandardDisplayString("_md5", children, ",");
     }
 }
