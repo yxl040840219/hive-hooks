c338e35d30ae197e38527edafee1bfa2bce9659d
yxl040840219 <yxl040840219@126.com>
2017-12-02 16:36:03 +0800

    update

--- src/main/java/com/customer/udf/SplitInt.java
+++ src/main/java/com/customer/udf/SplitInt.java
@@ -1,6 +1,8 @@
 package com.customer.udf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.IntWritable;
 
 import java.util.ArrayList;
 
@@ -9,11 +11,16 @@ import java.util.ArrayList;
  */
 public class SplitInt extends UDF {
 
-    public ArrayList<Integer> evaluate(final Object content, final String separator){
-        ArrayList<Integer> result = new ArrayList<Integer>();
+    public ArrayWritable evaluate(final Object content, final String separator){
+        ArrayWritable arrayWritable = new ArrayWritable(IntWritable.class) ;
+        ArrayList<IntWritable> result = new ArrayList<IntWritable>();
         for (String str : content.toString().split(separator.toString(), -1)) {
-            result.add(Double.valueOf(str).intValue());
+            IntWritable intWritable = new IntWritable();
+            intWritable.set(Double.valueOf(str).intValue());
+            result.add(intWritable);
         }
-        return result ;
+        IntWritable [] writables = new IntWritable[result.size()] ;
+        arrayWritable.set(result.toArray(writables));
+        return arrayWritable ;
     }
 }
